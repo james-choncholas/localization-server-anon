3cb87e41ee90a7267513ae897869c744325693c0
james-choncholas <jchoncholas@gmail.com>
2023-06-07 00:52:40 -0400

    networking scripts

---
+++ scripts/network_setup.sh
@@ -0,0 +1,8 @@
+export DEV="lo"
+export SPEED="1Gbit"
+export BURST="100000"
+export LAT="0msec"
+
+sudo tc qdisc del dev $DEV root
+sudo tc qdisc add dev $DEV root handle 1: tbf rate ${SPEED} burst ${BURST} limit ${BURST}
+sudo tc qdisc add dev $DEV parent 1:1 handle 10: netem delay ${LAT}
