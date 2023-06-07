8b50852d63eb3d9fa1d6aa3ff80d89096ece4d42
james-choncholas <jchoncholas@gmail.com>
2023-06-07 01:52:09 -0400

    fix aby benchmarking

--- test/aby-float/benchmark.cpp
+++ test/aby-float/benchmark.cpp
@@ -36,6 +36,150 @@ using namespace std;
 #define NUM_TRIALS 3
 #define SEED 0x666
 
+const std::string address = "127.0.0.1";
+constexpr const int port = 7766;
+constexpr const uint32_t nthreads = 1;
+constexpr const e_mt_gen_alg mt_alg = MT_OT;
+const seclvl slvl = LT;  // get_sec_lvl(secparam);
+const vector<e_sharing> ctypes = {/*S_ARITH,*/ S_BOOL,
+                                  S_YAO};  // no floats with arith
+map<int, string> cnames = {
+    {S_ARITH, "arith"},
+    {S_BOOL, "bool"},
+    {S_YAO, "yao"},
+};
+constexpr const uint32_t reservegates = 65536;
+constexpr const uint32_t bitlen = 32;
+static std::string get_circuit_dir() {
+  char result[PATH_MAX];
+  ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
+  const char* path;
+  if (count != -1) {
+    path = dirname(result);
+  } else {
+    std::cerr << "cant find circuit path\n";
+    assert(false);
+  }
+  std::string base_path = string(path) + "/../../extern/ABY/bin/circ";
+  return base_path;
+}
+
+void localize(e_role role, e_sharing sharing, cv::Mat rvec, cv::Mat tvec,
+              cv::Mat cameraMatrix, [[maybe_unused]] cv::Mat distCoeffs,
+              vector<cv::Point3f> objectPoints, vector<cv::Point2f> imagePoints,
+              auto secure_localize_func, vector<float>& res) {
+  float f = cameraMatrix.at<float>(0, 0);
+  float cx = cameraMatrix.at<float>(0, 2);
+  float cy = cameraMatrix.at<float>(1, 2);
+
+  ABYParty* party = new ABYParty(role, address, port, slvl, bitlen, nthreads,
+                                 mt_alg, reservegates, get_circuit_dir());
+  std::vector<Sharing*>& sharings = party->GetSharings();
+  Circuit* circ = sharings[sharing]->GetCircuitBuildRoutine();
+
+#if PPL_FLOW == PPL_FLOW_LOOP_LEAK || PPL_FLOW == PPL_FLOW_SiSL
+  //  always use boolean shares (not yao) so BuildAndRunLM can get raw shares.
+  //  this is because you cant use Y2B shares on Yao input gates.
+  Circuit* bc = sharings[S_BOOL]->GetCircuitBuildRoutine();
+#else
+  Circuit* bc = circ;
+#endif
+
+  assert(objectPoints.size() == imagePoints.size());
+  int numPts = objectPoints.size();
+
+  // Allocate space for shares
+  share** s_objectPoints = new share*[4 * numPts];
+  share** s_imagePoints = new share*[3 * numPts];
+  share* s_f;
+  share* s_cx;
+  share* s_cy;
+  share** s_x = new share*[6];
+
+  // Prepare inputs
+  if (role == SERVER) {
+    // float one=1.0;
+    for (int p = 0; p < numPts; p++) {
+      s_objectPoints[p] =
+          bc->PutINGate((uint32_t*)&objectPoints[p].x, bitlen, role);
+    }
+    for (int p = 0; p < numPts; p++) {
+      s_objectPoints[numPts + p] =
+          bc->PutINGate((uint32_t*)&objectPoints[p].y, bitlen, role);
+    }
+    for (int p = 0; p < numPts; p++) {
+      s_objectPoints[(2 * numPts) + p] =
+          bc->PutINGate((uint32_t*)&objectPoints[p].z, bitlen, role);
+    }
+    // for(int p=0; p<numPts; p++) {
+    //     s_objectPoints[3*numPts+p] = bc->PutINGate((uint32_t*) &one, bitlen,
+    //     role);
+    // }
+    for (int p = 0; p < numPts; p++) {
+      s_imagePoints[p] =
+          bc->PutINGate((uint32_t*)&imagePoints[p].x, bitlen, role);
+    }
+    for (int p = 0; p < numPts; p++) {
+      s_imagePoints[numPts + p] =
+          bc->PutINGate((uint32_t*)&imagePoints[p].y, bitlen, role);
+    }
+    // for(int p=0; p<numPts; p++) {
+    //     s_imagePoints[2*numPts+p] = bc->PutINGate((uint32_t*) &one, bitlen,
+    //     role);
+    // }
+    s_f = bc->PutINGate((uint32_t*)&f, bitlen, role);
+    s_cx = bc->PutINGate((uint32_t*)&cx, bitlen, role);
+    s_cy = bc->PutINGate((uint32_t*)&cy, bitlen, role);
+    //float zero = 0.0f;
+    for (int p = 0; p < 3; p++) {
+      s_x[p] = bc->PutINGate((uint32_t*)&rvec.at<float>(p), bitlen, role);
+    }
+    for (int p = 0; p < 3; p++) {
+      s_x[p + 3] = bc->PutINGate((uint32_t*)&tvec.at<float>(p), bitlen, role);
+    }
+  } else {
+    for (int p = 0; p < 3 * numPts; p++) {  // ignore constant 1s
+      s_objectPoints[p] = bc->PutDummyINGate(bitlen);
+    }
+    for (int p = 0; p < 2 * numPts; p++) {  // ignore constant 1s
+      s_imagePoints[p] = bc->PutDummyINGate(bitlen);
+    }
+    s_f = bc->PutDummyINGate(bitlen);
+    s_cx = bc->PutDummyINGate(bitlen);
+    s_cy = bc->PutDummyINGate(bitlen);
+    for (int p = 0; p < 6; p++) {
+      s_x[p] = bc->PutDummyINGate(bitlen);
+    }
+  }
+
+  secure_localize_func(s_objectPoints, s_imagePoints, numPts, s_f, s_cx, s_cy,
+                       s_x, (BooleanCircuit*)circ, party, role);
+
+  for (int i = 0; i < 6; i++) {
+    share* temp = s_x[i];
+    s_x[i] = bc->PutOUTGate(s_x[i], ALL);
+    delete temp;
+  }
+
+  party->ExecCircuit();
+
+  if (role == SERVER) {
+    cout << "secure result: ";
+  }
+  for (int i = 0; i < 6; i++) {
+    uint32_t* output;
+    uint32_t out_bitlen, out_nvals;
+    s_x[i]->get_clear_value_vec(&output, &out_bitlen, &out_nvals);
+    res[i] = *(float*)output;
+
+    if (role == SERVER) {
+      cout << *(float*)output << " ";
+    }
+  }
+
+  delete party;
+}
+
 int main(int argc, char** argv) {
   if (argc != 4) {
     MSG("Usage: %s <party number> <num frames> <max num pts>\n", argv[0]);
@@ -46,19 +190,6 @@ int main(int argc, char** argv) {
   int max_num_pts = atoi(argv[3]);
 
   MSG("\nStarting ABY...\n");
-  uint32_t /*secparam = 128,*/ nthreads = 1;
-  uint16_t port = 7766;
-  std::string address = "127.0.0.1";
-  e_mt_gen_alg mt_alg = MT_OT;
-  seclvl seclvl = LT;  // get_sec_lvl(secparam);
-
-  std::vector<e_sharing> ctypes = {/*S_ARITH,*/ S_BOOL,
-                                   S_YAO};  // cant do floats with arith
-  map<int, string> cnames = {
-      {S_ARITH, "arith"},
-      {S_BOOL, "bool"},
-      {S_YAO, "yao"},
-  };
 
   vector<cv::Point2f> imagePoints;
   vector<cv::Point3f> objectPoints;
@@ -158,51 +289,50 @@ int main(int argc, char** argv) {
 
           {
             cout << "testing gauss newton - " << cnames[S_BOOL] << "\n";
-            vector<float> initialGuessCopy = initialGuess;
+            vector<float> res = initialGuess;
             WALL_CLOCK(aby_bool_float_gn);
             WALL_TIC(aby_bool_float_gn);
-            // test_gaussnewton_circuit(role, address, port, seclvl, nthreads,
-            // mt_alg, S_BOOL,
-            //     objectPointsSubset, imagePointsSubset, f, cx, cy,
-            //     &initialGuessCopy[0]);
-
+            localize(role, S_BOOL, rvec, tvec, cameraMatrix, distCoeffs,
+                     objectPointsSubset, imagePointsSubset,
+                     BuildAndRunGaussNewton, res);
             float error[6];
             for (int e = 0; e < 6; e++) {
-              error[e] = initialGuessCopy[e] - gtpose[e];
+              error[e] = res[e] - gtpose[e];
             }
             float norm = twonormsq(error, 6);
             if (norm < GT_MIN_ER) {
               WALL_TOC_CSV_XY(aby_bool_float_gn, i);
               PrintSumTimings(objectPoints.size());
             } else
-              MSG("Not printing timing - not converged. Pose error norm: %f\n",
+              MSG("Not printing timing - not converged. Pose error norm: "
+                  "%f\n",
                   norm);
 
-            printVector("[rotation; translation]", &initialGuessCopy[0], 6);
+            printVector("[rotation; translation]", &res[0], 6);
             ClearSumTimings();
           }
 
           {
             cout << "testing gauss newton - " << cnames[S_YAO] << "\n";
-            vector<float> initialGuessCopy = initialGuess;
+            vector<float> res = initialGuess;
             WALL_CLOCK(aby_yao_float_gn);
             WALL_TIC(aby_yao_float_gn);
-            // test_gaussnewton_circuit(role, address, port, seclvl, nthreads,
-            // mt_alg, S_YAO,
-            //     objectPointsSubset, imagePointsSubset, f, cx, cy,
-            //     &initialGuessCopy[0]);
+            localize(role, S_YAO, rvec, tvec, cameraMatrix, distCoeffs,
+                     objectPointsSubset, imagePointsSubset,
+                     BuildAndRunGaussNewton, res);
             float error[6];
             for (int e = 0; e < 6; e++) {
-              error[e] = initialGuessCopy[e] - gtpose[e];
+              error[e] = res[e] - gtpose[e];
             }
             float norm = twonormsq(error, 6);
             if (norm < GT_MIN_ER) {
               WALL_TOC_CSV_XY(aby_yao_float_gn, i);
               PrintSumTimings(objectPoints.size());
             } else
-              MSG("Not printing timing - not converged. Pose error norm: %f\n",
+              MSG("Not printing timing - not converged. Pose error norm: "
+                  "%f\n",
                   norm);
-            printVector("[rotation; translation]", &initialGuessCopy[0], 6);
+            printVector("[rotation; translation]", &res[0], 6);
             ClearSumTimings();
           }
 #endif
@@ -219,45 +349,43 @@ int main(int argc, char** argv) {
 
           {
             cout << "testing lm - " << cnames[S_BOOL] << "\n";
-            vector<float> initialGuessCopy = initialGuess;
+            vector<float> res = initialGuess;
             WALL_CLOCK(aby_bool_float_lm);
             WALL_TIC(aby_bool_float_lm);
-            // test_lm_circuit(role, address, port, seclvl, nthreads, mt_alg,
-            // S_BOOL,
-            //     objectPointsSubset, imagePointsSubset, f, cx, cy,
-            //     &initialGuessCopy[0]);
+            localize(role, S_BOOL, rvec, tvec, cameraMatrix, distCoeffs,
+                     objectPointsSubset, imagePointsSubset, BuildAndRunLM, res);
             float error[6];
             for (int e = 0; e < 6; e++) {
-              error[e] = initialGuessCopy[e] - gtpose[e];
+              error[e] = res[e] - gtpose[e];
             }
             float norm = twonormsq(error, 6);
             if (norm < GT_MIN_ER)
               WALL_TOC_CSV_XY(aby_bool_float_lm, i);
             else
-              MSG("Not printing timing - not converged. Pose error norm: %f\n",
+              MSG("Not printing timing - not converged. Pose error norm: "
+                  "%f\n",
                   norm);
-            printVector("[rotation; translation]", &initialGuessCopy[0], 6);
+            printVector("[rotation; translation]", &res[0], 6);
           }
           {
             cout << "testing lm - " << cnames[S_YAO] << "\n";
-            vector<float> initialGuessCopy = initialGuess;
+            vector<float> res = initialGuess;
             WALL_CLOCK(aby_yao_float_lm);
             WALL_TIC(aby_yao_float_lm);
-            // test_lm_circuit(role, address, port, seclvl, nthreads, mt_alg,
-            // S_YAO,
-            //     objectPointsSubset, imagePointsSubset, f, cx, cy,
-            //     &initialGuessCopy[0]);
+            localize(role, S_YAO, rvec, tvec, cameraMatrix, distCoeffs,
+                     objectPointsSubset, imagePointsSubset, BuildAndRunLM, res);
             float error[6];
             for (int e = 0; e < 6; e++) {
-              error[e] = initialGuessCopy[e] - gtpose[e];
+              error[e] = res[e] - gtpose[e];
             }
             float norm = twonormsq(error, 6);
             if (norm < GT_MIN_ER)
               WALL_TOC_CSV_XY(aby_yao_float_lm, i);
             else
-              MSG("Not printing timing - not converged. Pose error norm: %f\n",
+              MSG("Not printing timing - not converged. Pose error norm: "
+                  "%f\n",
                   norm);
-            printVector("[rotation; translation]", &initialGuessCopy[0], 6);
+            printVector("[rotation; translation]", &res[0], 6);
           }
 #endif
         }
